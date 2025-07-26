58a5dadbe1a1d7bc5c3b28a4e0ddf72e88f59e16
Leonardo Mayca <leomt8440@gmail.com>
2025-07-26 19:27:45 -0300

    95. Seguir Usuário

--- api/src/repositories/usuarios.go
+++ api/src/repositories/usuarios.go
@@ -150,3 +150,20 @@ func (repositorio usuarios) BuscarPorEmail(email string) (modelos.Usuario, error
 
 	return usuario, nil
 }
+
+// Seguir permite que um usuário siga outro
+func (repositorio usuarios) Seguir(usuarioID uint64, seguidorID uint64) error {
+	statement, erro := repositorio.db.Prepare(
+		"insert ignore into seguidores (usuario_id, seguidor_id) values (?, ?)",
+	)
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(usuarioID, seguidorID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
