cff7ad58e153e6c7e3b3d759dba60ba2d4bd5ee7
Leonardo Mayca <leomt8440@gmail.com>
2025-08-03 21:45:43 -0300

    108. Busca de Publicações de um Usuário

--- api/src/repositories/publicacoes.go
+++ api/src/repositories/publicacoes.go
@@ -140,3 +140,40 @@ func (repositorio Publicacoes) Deletar(publicacaoID uint64) error {
 
 	return nil
 }
+
+// BuscarPorUsuario traz todas as publicações de um usuário específico
+func (repositorio Publicacoes) BuscarPorUsuario(usuarioID uint64) ([]modelos.Publicacao, error) {
+	linhas, erro := repositorio.db.Query(`
+		select p.*, u.nick from publicacoes p
+		inner join usuarios u on u.id = p.autor_id
+		where p.autor_id = ?
+		order by p.criadoEm desc`,
+		usuarioID,
+	)
+	if erro != nil {
+		return nil, erro
+	}
+	defer linhas.Close()
+
+	var publicacoes []modelos.Publicacao
+
+	for linhas.Next() {
+		var publicacao modelos.Publicacao
+
+		if erro = linhas.Scan(
+			&publicacao.ID,
+			&publicacao.Titulo,
+			&publicacao.Conteudo,
+			&publicacao.AutorID,
+			&publicacao.Curtidas,
+			&publicacao.CriadoEm,
+			&publicacao.AutorNick,
+		); erro != nil {
+			return nil, erro
+		}
+
+		publicacoes = append(publicacoes, publicacao)
+	}
+
+	return publicacoes, nil
+}
